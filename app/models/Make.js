1a71d0fe0dcc417ec8422f5b2ca8afae3ca478ca
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-11 01:00:43 +0500

    check

--- app/models/Make.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const MakeSchema = mongoose.Schema({
-  make: { type: String, required: true },
-});
-const MakeModel =
-  mongoose.models.Make || mongoose.model("Make", MakeSchema);
-
-export default MakeModel;
--- app/models/connectDb.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export default async function connectDb() {
-  await mongoose.connect(
-    "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
-  );
-}
