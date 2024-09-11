3459144730e1330c4cc92e8dbd708638cb5a1a66
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-11 09:38:10 +0500

    fvsdf

--- app/registration/connectDb.js
+++ app/registration/connectDb.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
-export default async () => {
+const connectDb = async () => {
   await mongoose.connect(
-    "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
+    // "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
+    "mongodb+srv://dentalBrando:DpTXETcSqu7P64p@dentalbrandoproposalmak.ugsf2qa.mongodb.net/?retryWrites=true&w=majority&appName=dentalbrandoproposalmaker"
   );
 };
+
+export default connectDb;
