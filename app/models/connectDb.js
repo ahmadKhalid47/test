e65d259bfefadecc8f0ee62b89f15127f3711bcb
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-11 09:23:35 +0500

    nnkjnk

--- app/models/connectDb.js
+++ app/models/connectDb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   await mongoose.connect(
-    "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
+    // "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
+    "mongodb+srv://dentalBrando:DpTXETcSqu7P64p@dentalbrandoproposalmak.ugsf2qa.mongodb.net/?retryWrites=true&w=majority&appName=dentalbrandoproposalmaker"
   );
 };
 
